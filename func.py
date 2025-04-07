ebb627e25fc403dd8166438c55098208291eb686
Vinicius Araújo da Silva <vinicius.araujo@conectrom.local>
2025-04-07 15:18:07 -0300

    Remoção de algumas funções e adição de comentários

--- func.py
+++ func.py
@@ -6,56 +6,7 @@ import tkinter as tk
 from tkinter import Tk
 from tkinter import filedialog
 from pathlib import Path
-from tkinter.filedialog import askopenfilename
-
-
-def local(titulo='Selecione um arquivo'):
-    root = Tk()
-    root.withdraw()
-    caminho = askopenfilename(title= titulo)
-    root.destroy()
-    return caminho
-
-def diretorio():
-    caminho = os.getcwd()
-    caminho = caminho.replace("\\","/")
-    return caminho
-
-#! Função para salvar a tabela de locação num csv
-def escrever_csv(caminho,   # Caminho do arquivo que vai receber esses valores
-                  dados     # Informações colhidas da tabela de locação
-                  ): 
-    with open(caminho, 'w') as f: # Recebe o local onda vai ser salvo e que vai escrever nesse arquivo
-        for row in dados: # Dados vai ser uma lista com listas dentro e cada lista representa uma linha da tabela de locação
-            '''
-            No código antigo, depois de todo valor da lista era inserida uma vírgula e,
-            quando ia pegar essa arquivo para consulta, uma nova coluna com nenhum valor
-            aparecia justamente por todo valor ser acompanhado por uma vírgulo. Por isso
-            foi inserido esse contador para que o último valor da linha não seja acompanhado
-            com a vírgulo e não ter essa coluna vazia extra
-            '''
-            count  = 0 
-            for item in row: # Outro for com cada item da linha da tabela de locação
-                count = count + 1
-                '''
-                Existe um problema com os elementos que são números e, principalmente float.
-                As casas decimais desses números crescem muito e o método open() só consegue
-                escrever valores que são str. Para resolver isso, arredonda os valores que 
-                são float e transforma tudos os valores para str.
-                '''
-                if isinstance(item, float): 
-                    item = round(item, 2)
-                # Open() também não consegue lidar com NaN, então, transforma-se para str com " " como valor
-                if item is None:
-                    f.write(" "+",")
-                
-                # Verificação do último valor da linha
-                if count != len(row):
-                    f.write(str(item)+",")
-                else:
-                    f.write(str(item))
-            f.write("\n") # No final de cada linha da tabela de locação, dá uma quebra de linha
-
+from funcoes_acessorias import local, diretorio, escrever_csv
 
 def pegar_postes():
     '''
@@ -79,13 +30,14 @@ def pegar_postes():
     while True:
         # Pegar o caminho do arquivo da tabela de locação que vai ser utilizada
         caminho = local('Selecione a tabela de locação')
-
+        # 1 - Primeiro ponto verificado 
         if not caminho:
             print('Não foi selecionado nenhum arquivo!!!')
             return print('Solicitação encerrada, chame novamente!!!')
         
         count = 0
         if caminho:
+            # 2 - Segundo ponto verificado
             if not '.xlsx' in caminho:
                 print('O documento escolhido não é uma arquivo .xlsx')
                 return print('Solicitação encerrada, chame novamente!!!') 
@@ -94,7 +46,7 @@ def pegar_postes():
             for sheet in wb_teste.sheetnames:
                 if 'TABELA DE LOCAÇÃO' in sheet:
                     count = count + 1
-
+        # 3 - Terceiro ponto verificado
         if count > 0:
             break
         elif count == 0:
